cb62ca17982a736bee705744cd43d45b178e1fca
Sriharan-official <113430198+Sriharan-illiquidLabs@users.noreply.github.com>
2023-01-17 18:01:03 +0530

    //

--- Assets/OtherDevs/Arvind/Scripts/Bonk_Scripts/SpawnPlayer_AR.cs
+++ Assets/OtherDevs/Arvind/Scripts/Bonk_Scripts/SpawnPlayer_AR.cs
@@ -7,18 +7,53 @@ public class SpawnPlayer_AR : MonoBehaviour
 
     public Transform[] spawnPoints;
 
-    public GameObject player;
+    public GameObject[] player;
     public TextMeshProUGUI coinUI;
     public TextMeshProUGUI multiplierUI;
+
+    public GameObject current;
+    bool dog;
+
+    bool ok;
     void Start()
     {
-        GameObject player1 = Instantiate(player, spawnPoints[Random.RandomRange(0, 4)].transform.position, Quaternion.identity);
+       
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1 ) && !ok)
+        {
+            GameStart(0);
+               
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2) && !ok)
+        {
+            GameStart(1);
+           
+        }
+
+    }
+
+    void GameStart(int i)
+    {
+        GameObject player1 = Instantiate(player[i], spawnPoints[Random.RandomRange(0, 4)].transform.position, Quaternion.identity);
+        current = player1;
         Cinemachine.CinemachineFreeLook gm = GameManager.instance.cine.GetComponent<Cinemachine.CinemachineFreeLook>();
         gm.Follow = player1.transform;
         gm.LookAt = player1.transform;
         player1.GetComponent<Cheems>().coinCount = coinUI;
         player1.GetComponent<Cheems>().multiplierText = multiplierUI;
+        ok = true;
+        LeanTween.delayedCall(10f, Restart);
     }
+
+
+    void Restart()
+    {
+        ok = false;
+    } 
 
 
 
