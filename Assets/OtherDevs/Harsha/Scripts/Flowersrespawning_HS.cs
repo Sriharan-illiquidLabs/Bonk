6a8cea6da36daa47476a7d860b518f84b3ef565a
HarshaKarteek <harshakarteek007@gmail.com>
2023-01-18 12:24:45 +0530

    fence collision bug fix and flowers optimization fix

--- Assets/OtherDevs/Harsha/Scripts/Flowersrespawning_HS.cs
+++ Assets/OtherDevs/Harsha/Scripts/Flowersrespawning_HS.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Flowersrespawning : MonoBehaviour
+public class Flowersrespawning_HS: MonoBehaviour
 {
     public float respawntime = 5f;
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
+        Debug.Log("hit");
         if (collision.gameObject.tag == "Player")
         {
             gameObject.SetActive(false);
